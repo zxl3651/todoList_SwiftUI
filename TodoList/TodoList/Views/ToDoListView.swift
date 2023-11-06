e688cf7208b0a020214ac4cbab13824b4113ae20
zxl3651 <zxl3651@naver.com>
2023-11-06 22:19:24 +0900

    [feat] Views - .ToDoListView (userId를 통한 Firebase 접근 및 Data 가져오기)

--- TodoList/TodoList/Views/ToDoListView.swift
+++ TodoList/TodoList/Views/ToDoListView.swift
@@ -5,22 +5,27 @@
 //  Created by 이성현 on 2023/11/05.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
     
     @StateObject var viewModel = ToDoListViewViewModel()
-    
-    private var userId : String
+    @FirestoreQuery var items: [ToDoListItem]
     
     init(userId: String) {
-        self.userId = userId
+        // users/<id>/todos/<entries> 여기서 id에 해당하는게 userId이므로 필요하다!
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos")
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    Text(item.title)
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -30,13 +35,13 @@ struct ToDoListView: View {
                     Image(systemName: "plus")
                 })
             }
-            .sheet(isPresented: $viewModel.showingNewItemView, content: {
+            .sheet(isPresented: $viewModel.showingNewItemView) {
                 NewItemView(newItemPresented: $viewModel.showingNewItemView)
-            })
+            }
         }
     }
 }
 
 #Preview {
-    ToDoListView(userId: "")
+    ToDoListView(userId: "Jtr8zCLTxUXWVUcx9p3VHcaMaRr2")
 }
