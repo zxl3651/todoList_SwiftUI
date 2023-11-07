6d5f9e91866f69a4f1cae3ddde3e2c5ee6facbf2
zxl3651 <zxl3651@naver.com>
2023-11-07 14:55:59 +0900

    [fix] Views - .ToDoListItemView (기존 ProfileViewViewModel() 에서 ToDoListItemViewViewModel() 로 변경), - .ProfileView (초기 틀 제작),  ViewModels - .ProfileViewViewModel (기존 toggleIsDone() 함수 이동, logout() 함수 생성), .ToDoListItemViewViewModel(toggleIsDone() 함수 이동 완료)

--- TodoList/TodoList/ViewModels/ToDoListItemViewViewModel.swift
+++ TodoList/TodoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,9 +5,29 @@
 //  Created by 이성현 on 2023/11/05.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// ViewModel for single to do list item view (each row in items list)
 class ToDoListItemViewViewModel : ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
+    
 }
--- TodoList/TodoList/Views/ProfileView.swift
+++ TodoList/TodoList/Views/ProfileView.swift
@@ -13,6 +13,33 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
+                // Avatar
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.blue)
+                    .frame(width: 125, height: 125)
+                
+                // Info: Name, Email, Member since
+                VStack (alignment: .leading) {
+                    HStack {
+                        Text("Name")
+                        Text("lee seong hyeon")
+                    }
+                    HStack {
+                        Text("Email")
+                        Text("lee seong hyeon")
+                    }
+                    HStack {
+                        Text("Member Since: ")
+                        Text("lee seong hyeon")
+                    }
+                }
+                
+                // Sign out
+                TLButtonView(title: "Log Out", background: .red) {
+                    viewModel.logout()
+                }
                 
             }
             .navigationTitle("Profile")
